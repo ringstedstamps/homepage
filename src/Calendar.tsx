de253e8810da24c0c5b9e59467051978e10e5d70
ringstedstamps <johnpetersen1959@gmail.com>
2025-07-14 12:14:17 +0200

    ok

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -13,115 +13,124 @@ function Calendar() {
       <div style={styles.textContainer}>
         {/* <span style={styles.text}> */}
         <p style={{ fontSize: 30 }}>
-          Forår 2025
+          Efterår 2025
         </p>
         <p style={{ fontSize: 20 }}>
           Mødetid: kl. 19.00-22.00 - hvis ikke andet er nævnt
           <br />
           <span style={{ fontSize: 20 }}>
-          Selve dagens aktiviteter startes kl. 19.00
-          </span>    
-        </p>
-       
-
-        <p style={{ fontSize: 18, fontWeight: "bold"  }}>
-          06.01.2025
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-          - Sæsonstart, Nyt over Julen
-          </span>          
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
-          </span>
-        </p>
-        
-        
-        <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        20.01.2025
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-          - Vi ser på vores Ringsted-samling
-          </span>          
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
+            Selve dagens aktiviteter startes kl. 19.00
           </span>
         </p>
 
-        <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        03.02.2025
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-          - Filateliaften
-          </span>          
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
-          </span>
-        </p>
-        
 
         <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        17.02.2025
+          08.09.2025
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-          - Bankospil
-          </span>          
+           Opstart nyt over sommeren samt miniauktion.
+          </span>
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
+
           </span>
         </p>
-       
+
+
         <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        03.03.2025
+          22.09.2025
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-          - Generalforsamling
-          </span>          
+            Fremlægning af Ringsted samlingen v/Erik A.  Miniauktion.
+          </span>
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
+          
           </span>
         </p>
 
         <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        17.03.2025
+          06.10.2025
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-          - Filateliaften
-          </span>          
+           Filateliaften, vi ser på tofarvede med ret og omvendte rammer mm.  Miniauktion.
+          </span>
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-            - Bytte og miniauktion
+          
           </span>
         </p>
-        
-        <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        31.03.2025
-          <br />
-          <span style={{ fontSize: 15, color: "Red" }}>
-          - Storauktion
-          </span>          
-          <br />         
-        </p>
-       
+
 
         <p style={{ fontSize: 18, fontWeight: "bold" }}>
-        14.04.2025
+          19.10.2025
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-          - Sæsonafslutning
-          </span>          
+           Fælles Byttedag i SORØ.
+          </span>
           <br />
           <span style={{ fontSize: 15, color: "Red" }}>
-          - Stor-bytteaften
           </span>
         </p>
-        
 
-        
+        <p style={{ fontSize: 18, fontWeight: "bold" }}>
+          20.10.2025
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+           Filateliaften, adskillelse af bundtryk på tidlige danske mærker.  Miniauktion
+          </span>
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+          </span>
+        </p>
+
+        <p style={{ fontSize: 18, fontWeight: "bold" }}>
+          03.11.2025
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+            Bankospil og miniauktion.
+          </span>
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+          </span>
+        </p>
+
+        <p style={{ fontSize: 18, fontWeight: "bold" }}>
+          17.11.2025
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+            Filateliaften, Emne, Skibe  samt miniauktion.
+          </span>
+          <br />
+        </p>
+
+
+        <p style={{ fontSize: 18, fontWeight: "bold" }}>
+          01.12.2025
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+            Storauktion.
+          </span>
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+           
+          </span>
+        </p>
+
+        <p style={{ fontSize: 18, fontWeight: "bold" }}>
+          15.12.2025
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+            Juleafslutning med spisning og bankospil.
+          </span>
+          <br />
+          <span style={{ fontSize: 15, color: "Red" }}>
+           
+          </span>
+        </p>
+
+
+
 
         <br />
         <p style={{ fontSize: 18 }}>
